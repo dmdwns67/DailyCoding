b1196ae8ae50b5967185c7f45de3998519d3752b
dmdwns67 <dmdwns67@naver.com>
2020-07-27 15:01:58 +0900

    개선

--- src/bak5585/Main.java
+++ src/bak5585/Main.java
@@ -4,22 +4,27 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int[] coins = {500, 100, 50, 10, 5, 1};
-
         int pay = sc.nextInt();
         int change = 1000 - pay;
-
         int cnt = 0;
+
+        for(int coin : new int[]{500, 100, 50, 10, 5, 1}){
+            cnt += change / coin;
+            change %= coin;
+        }
+
+        /*
+        int[] coins = {500, 100, 50, 10, 5, 1};
+
         while(change != 0){
             for(int i=0; i<coins.length; i++){
                 while(coins[i] <= change){
-                    //System.out.println(coins[i]+"엔 차감");
                     change = change - coins[i];
                     cnt++;
-                    //System.out.println("현재 change: " + change +", cnt: "+cnt);
                 }
             }
         }
+        */
 
         System.out.println(cnt);
 
