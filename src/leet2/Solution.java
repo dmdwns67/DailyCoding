6565f362db06a8e9994f109d21f695dc8e2e8f81
dmdwns67 <dmdwns67@naver.com>
2021-11-17 22:45:41 +0900

    DailyCoding LeetCode 2[구현]

---
+++ src/leet2/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode answer = new ListNode();
+        ListNode cursor = answer;
+
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int sum = 0;
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            ListNode node = new ListNode(sum);
+            cursor.next = node;
+            cursor = cursor.next;
+        }
+        if(carry > 0) {
+            ListNode node = new ListNode(carry);
+            cursor.next = node;
+        }
+        return answer.next;
+    }
+}
